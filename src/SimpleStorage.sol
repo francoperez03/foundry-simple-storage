36f94c4254a45f3685f4be0bc9974d8d83deabf4
Fran <franco.perez03@gmail.com>
2024-04-13 11:24:01 -0300

    feat: add new contract

---
+++ src/SimpleStorage.sol
@@ -0,0 +1,29 @@
+//SPDX-LicenseIdentifier: MIT
+
+pragma solidity 0.8.19;
+
+contract SimpleStorage {
+    uint myFavoriteNumber;
+
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPeople;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns uint256 {
+        return myFavoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber
+    }
+}
